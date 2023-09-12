eeb66857f354febf07700caf32a4bebbc32f9cc4
muriel-s <muriel.liberatore@gmail.com>
2023-09-12 18:56:39 -0400

    started base of alien

--- activities/02-draw-an-alien/js/script.js
+++ activities/02-draw-an-alien/js/script.js
@@ -1,9 +1,8 @@
 /**
- * Title of Project
- * Author Name
+ * Alien Drawing
+ * Muriel Smith
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * Muriel's drawing of an alien.
  */
 
 "use strict";
@@ -20,6 +19,26 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    // draws background
+    createCanvas(640,480);
+    background ("#d9b2bb");
+
+    // draws the body of the alien
+    rectMode(CENTER)
+    noStroke();
+    fill(245);
+    rect(320,240,200,150,80,80,80,80);
+
+    //draws the head of the alien
+    ellipse(195,150,90,90);
+
+    //draws the neck of the alien
+    beginShape();
+    vertex(238,137); //first anchor point
+    bezierVertex(245,150,270,165,290,166);
+    vertex(222,250);
+    bezierVertex(220,230,210,195,185,193);
+    endShape(CLOSE);
 
 }
 
