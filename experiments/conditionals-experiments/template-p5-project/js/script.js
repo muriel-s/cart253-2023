ab6a5fe8489635f2e2007592c438896599fef0dc
muriel-s <muriel.liberatore@gmail.com>
2023-09-28 12:15:56 -0400

    conditionals experiments: finished 4.7

--- experiments/conditionals-experiments/template-p5-project/js/script.js
+++ experiments/conditionals-experiments/template-p5-project/js/script.js
@@ -2,45 +2,53 @@
  * Conditionals experiments
  * Muriel Smith
  * 
- * This is my coding experimentation
+ * This is my sandbox
  * file while I learn about conditionals.
  */
 
 "use strict";
 
-let caterpillar = {
-    x: 100,
+let circle = {
+    x: 250,
     y: 250,
-    segmentSize: 50
-}
-
+    size: 100,
+    vx: 0,
+    vy: 0,
+    ax: 0,
+    ay: 0,
+    acceleration: 0.25,
+    maxSpeed: 3
+};
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(500,500);
 }
 
 
 function draw() {
     background(0);
-    noStroke();
-    fill(100,200,100);
 
-    // let x = caterpillar.x;
-    // let numSegments = 5;
-    // let segmentsDrawn = 0;
-
-    // while (segmentsDrawn < numSegments) {
-    //     ellipse(x, caterpillar.y, caterpillar.segmentSize);
-    //     x = x + 40;
-    //     segmentsDrawn++;
-    // }
-
-    let x = caterpillar.x;
-    let numSegments = 10;
-
-    for (let segmentsDrawn = 0; segmentsDrawn < numSegments; segmentsDrawn++) {
-        ellipse(x, caterpillar.y, caterpillar.segmentSize);
-        x = x+ 40;
+    if (mouseX < circle.x) {
+        circle.ax = -circle.acceleration;
+    }
+    else {
+        circle.ax = circle.acceleration;
     }
 
+    if (mouseY < circle.y) {
+        circle.ay = -circle.acceleration;
+    }
+    else {
+        circle.ay = circle.acceleration;
+    }
+
+    circle.vx = circle.vx + circle.ax;
+    circle.vx = constrain(circle.vx, -circle.maxSpeed, circle.maxSpeed);
+    circle.vy = circle.vy + circle.ay;
+    circle.vy = constrain(circle.vy, -circle.maxSpeed, circle.maxSpeed);
+
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+
+    ellipse(circle.x, circle.y, circle.size);
 }
