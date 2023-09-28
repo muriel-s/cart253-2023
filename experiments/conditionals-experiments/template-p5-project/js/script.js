808ae18629582fc903e2a8859708b453489cf992
muriel-s <muriel.liberatore@gmail.com>
2023-09-28 00:03:35 -0400

    got through 4.3

--- experiments/conditionals-experiments/template-p5-project/js/script.js
+++ experiments/conditionals-experiments/template-p5-project/js/script.js
@@ -8,19 +8,17 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let caterpillar = {
+    x: 100,
+    y: 250,
+    segmentSize: 50
 }
-
 
 /**
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500, 500);
 }
 
 
@@ -28,5 +26,18 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
+    noStroke();
+    fill(100,200,100);
+
+    let x = caterpillar.x;
+    let numSegments = 5;
+    let segmentsDrawn = 0;
+
+    while (segmentsDrawn < numSegments) {
+        ellipse(x, caterpillar.y, caterpillar.segmentSize);
+        x = x + 40;
+        segmentsDrawn += 1;
+    }
 
 }
