476a103c4b21a601e51b2510f92c460faa189d41
muriel-s <muriel.liberatore@gmail.com>
2023-10-03 19:32:45 -0400

    finished functions-experiments

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -2,47 +2,79 @@
  * Functions experiments
  * Muriel
  * 
- * 
+ * This is my basic HSB colorpicker made while watching the Functions lectures.
  */
 
 "use strict";
 
-
-/**
- * Description of preload
-*/
-function preload() {
-
+let bg = {
+    hue: 0,
+    saturation: 50,
+    brightness: 50,
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
-    createCanvas(500,500);
+    createCanvas(windowWidth,windowHeight);
+    colorMode(HSB);
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
-    background(0);
+    background(bg.hue, bg.saturation, bg.brightness);
 
-    // parallels(300, 50, 20, 1, 50, 10);
-    // parallels(200, 150, 4, 2, 100, 20);
-    // parallels(100, 250, 10, 3, 20, 30);
-    parallels(random(100, 300),random(50, 250), random(4, 20), random(1,3), random(20, 100), random(10, 30));
+    displayHSBvalues();
+    displayInstructions();
+}
+
+function displayHSBvalues() {
+    fill(255);
+    textFont('Roboto Mono');
+    textStyle('Regular 400');
+    textSize(32);
+    textAlign(CENTER,CENTER);
+    text(`H: ${bg.hue}  S: ${bg.saturation}  B: ${bg.brightness}\n`, width/2, height/2);
+}
+
+function displayInstructions() {
+    textStyle('Thin 100');
+    textSize(16);
+    textAlign(LEFT,TOP);
+    text(`directions:\na + z keys change hue ("H")\ns + x keys change saturation ("S")\nd + c keys change brightness ("B")`, 50, 50); 
 }
 
 
-function parallels(x, y, numLines, lineWidth, lineHeight, lineSpacing) {
-    for (let i = 0; i < numLines; i++) {
-        noStroke();
-        fill(255);
-        rectMode(CENTER);
-        rect(x, y, lineWidth, lineHeight);
-        x = x + lineSpacing; 
+function keyPressed() {
+    if (key === `a`) {
+        changeHue(10);
     }
+    else if (key === `z`) {
+        changeHue(-10);
+    }
+    else if (key === `s`) {
+        changeSaturation(5);
+    }
+    else if (key === `x`) {
+        changeSaturation(-5);
+    }
+    else if (key === `d`) {
+        changeBrightness(5);
+    }
+    else if (key === `c`) {
+        changeBrightness(-5);
+    }
+}
+
+function changeHue(increment) {
+    bg.hue = bg.hue + increment;
+    bg.hue = constrain(bg.hue, 0, 360);
+}
+
+function changeSaturation(increment) {
+    bg.saturation = bg.saturation + increment;
+    bg.saturation = constrain(bg.saturation, 0, 100);
+}
+
+function changeBrightness(increment) {
+    bg.brightness = bg.brightness + increment;
+    bg.brightness = constrain(bg.brightness, 0, 100);
 }
