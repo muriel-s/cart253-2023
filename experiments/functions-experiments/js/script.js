df4d8650221e9169335d22d16c98e39f9bf4aa06
muriel-s <muriel.liberatore@gmail.com>
2023-10-03 16:16:46 -0400

    functions experiments: finished 5.1

--- experiments/functions-experiments/js/script.js
+++ experiments/functions-experiments/js/script.js
@@ -1,12 +1,20 @@
 /**
- * Title of Project
- * Author Name
+ * Functions experiments
+ * Muriel
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * 
  */
 
 "use strict";
+
+let circle = {
+    x: 0,
+    y: 250,
+    size: 100,
+    vx: 5,
+    vy: 0,
+    hue: 1,
+}
 
 /**
  * Description of preload
@@ -20,7 +28,8 @@ function preload() {
  * Description of setup
 */
 function setup() {
-
+    createCanvas(500,500);
+    colorMode(HSB);
 }
 
 
@@ -28,5 +37,41 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+    background(0);
 
+    move();
+    wrap();
+    display();
+}
+
+
+function move() {
+    circle.x = circle.x + circle.vx;
+    circle.y = circle.y + circle.vy;
+}
+
+
+function wrap() {
+    if (circle.x > width) {
+        reset();
+    }
+}
+
+
+function display() {
+    fill(circle.hue, 80, 80);
+    ellipse(circle.x, circle.y, circle.size);
+}
+
+
+function reset() {
+    circle.x = 0;
+    circle.hue ++;
+    circle.vx = circle.vx + 1;
+    circle.size = circle.size ++;
+}
+
+
+function mousePressed() {
+    reset();
 }
