1095fbded672fc9e45a15a975249f168d9190219
muriel-s <muriel.liberatore@gmail.com>
2023-10-23 20:13:04 -0400

    finished debugging activity

--- activities/debugging/js/script.js
+++ activities/debugging/js/script.js
@@ -1,32 +1,102 @@
 /**
- * Title of Project
- * Author Name
+ * Debugging Activity
+ * Muriel Smith
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let user = {
+    x: 0,
+    y: 0,
+    size: 100
+};
+ 
+// Food objects
+let food1;
+let food2;
+let food3;
+let food4;
+let food5;
+let food6;
 
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
+    createCanvas(600, 600);
 
+    food1 = createFood(50, height/2);
+    food2 = createFood(150, height/2);
+    food3 = createFood(250, height/2);
+    food4 = createFood(350, height/2);
+    food5 = createFood(450, height/2);
+    food6 = createFood(550, height/2);
 }
 
+function createFood(x, y) {
+    let food = {
+        x: x,
+        y: y,
+        size: 50,
+        eaten: false
+    };
+    return food;
+}
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(0);  
+    
+    // Move the user (with the mouse)
+    moveUser(); 
+    
+    // Check whether the user has eaten either food
+    checkFood(food1);
+    checkFood(food2);
+    checkFood(food3);
+    checkFood(food4);
+    checkFood(food5);
+    checkFood(food6);
+    
+    // Display the user and foods
+    displayUser();
+    displayFood(food1);
+    displayFood(food2);
+    displayFood(food3);
+    displayFood(food4);
+    displayFood(food5);
+    displayFood(food6);
+}
 
+// Sets the user position to the mouse position
+function moveUser() {
+    user.x = mouseX;
+    user.y = mouseY;
+}
+
+// Marks food as eaten when it is intersected with the mouse
+function checkFood(food) {
+    if (!food.eaten) {
+        let d = dist(user.x, user.y, food.x, food.y);
+        if (d < user.size / 2 + food.size / 2) {
+            food.eaten = true;
+        }
+    }
+}
+
+// Draw the user as a circle
+function displayUser() {
+    push();
+    fill(255);
+    ellipse(user.x, user.y, user.size);
+    pop();
+}
+
+// Draw the food as a circle
+function displayFood(food) {
+  // Check if the food is still available to be eaten
+  if (!food.eaten) {
+    // Display the food as its position and with its size
+    push();
+    fill(255, 100, 100);
+    ellipse(food.x, food.y, food.size);
+    pop();
+  }
 }
