a9a667f53b61f006f77cff2ad2e9503968e0133b
muriel-s <muriel.liberatore@gmail.com>
2023-10-23 20:40:38 -0400

    E4: did base of exercise

--- exercises/age-of-aquariums/js/script.js
+++ exercises/age-of-aquariums/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Age of Aquariums
+ * Muriel Smith
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -8,25 +8,62 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
+let school = [];
+let schoolSize = 10;
 
-}
-
-
-/**
- * Description of setup
-*/
 function setup() {
+    createCanvas(600,600);
 
+    for (let i = 0; i < schoolSize; i++) {
+        let fish = createFish(random(0, width), random(0,height));
+        school.push(fish);
+    }
 }
 
+function createFish(x, y) {
+    let fish = {
+        x: x,
+        y: y,
+        size: 50,
+        vx: 0,
+        vy: 0,
+        speed: 2
+    };
+    return fish;
+}
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(0);
 
+    for (let i = 0; i < school.length; i++) {
+        moveFish(school[i]);
+        displayFish(school[i]);
+    }
+}
+
+function moveFish(fish) {
+    let change = random(0,1);
+    if (change < 0.05) {
+        fish.vx = random(-fish.speed, fish.speed);
+        fish.vy = random(-fish.speed, fish.speed);
+    }
+
+    fish.x = fish.x + fish.vx;
+    fish.y = fish.y + fish.vy;
+
+    fish.x = constrain(fish.x, 0, width);
+    fish.y = constrain(fish.y, 0, height);
+}
+
+function displayFish(fish) {
+    push();
+    fill(200, 100, 100);
+    noStroke();
+    ellipse(fish.x, fish.y, fish.size); 
+    pop();
+}
+
+function mousePressed() {
+    let fish = createFish(mouseX, mouseY);
+    school.push(fish);
 }
