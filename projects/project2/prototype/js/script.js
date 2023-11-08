095502de91ac041a19f189cfd4f06a9bfc5dd193
muriel-s <muriel.liberatore@gmail.com>
2023-11-08 16:24:36 -0500

    P2: added layers to noise and to circle

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -20,7 +20,7 @@ let bg = {
 
 // noise properties
 let noise = {
-    amount: 1000,
+    amount: 2000,
     hue: 180,
     saturation: 0,
     brightness: 100,
@@ -38,25 +38,26 @@ let circles = {
     y: 0,
     size: 0,
     minSize: 250,
-    maxSize: 750,
+    maxSize: 1000,
     spacing: 0,
-    amount: 4
+    amount: 1,
+    variation: 20
 }
 
-// line pattern properties
-let lines = {
-    thickness: 2,
-    h: 180,
-    s: 100,
-    b: 100,
-    x: 0,
-    y: 0,
-    height: 0,
-    minHeight: 500,
-    maxHeight: 1000,
-    spacing: 0,
-    variation: 10
-}
+// // line pattern properties
+// let lines = {
+//     thickness: 2,
+//     h: 180,
+//     s: 100,
+//     b: 100,
+//     x: 0,
+//     y: 0,
+//     height: 0,
+//     minHeight: 500,
+//     maxHeight: 1000,
+//     spacing: 0,
+//     variation: 10
+// }
 
 function setup() {
     createCanvas(1000, 1000);
@@ -68,35 +69,84 @@ function draw() {
     // draws background
     background(bg.h, bg.s, bg.b);
 
+    // // draws medium ellipse pattern
+    // circles.size = lerp(circles.minSize, circles.maxSize, 0.5);
+    // circles.spacing = circles.thickness*20;
+    // for (let i = 0; i < circles.amount; i++) {
+    //     push();
+    //     noFill();
+    //     strokeWeight(2*circles.thickness);
+    //     let hue = lerp(circles.h, bg.h, 0,5);
+    //     let saturation = lerp(circles.s, bg.s, 0,5);
+    //     let brightness = lerp(circles.b, bg.b, 0,5);
+    //     stroke(hue, saturation, brightness);
+    //     circles.x = width/2;
+    //     circles.y = height/2;
+    //     circles.size = randomGaussian(circles.size, circles.size/circles.variation);
+    //     circles.size = constrain(circles.size, circles.minSize, circles.maxSize);
+    //     ellipse(circles.x, circles.y, circles.size);
+    //     pop();
+    // }
+
     // draws ellipse pattern
     circles.size = lerp(circles.minSize, circles.maxSize, 0.5);
-    circles.spacing = circles.thickness*200;
+    circles.spacing = circles.thickness*20;
     for (let i = 0; i < circles.amount; i++) {
         push();
         noFill();
-        strokeWeight(circles.thickness);
-        stroke(circles.h, circles.s, circles.b);
         circles.x = width/2;
         circles.y = height/2;
-        circles.size = randomGaussian(circles.size, circles.size/10);
+        circles.size = randomGaussian(circles.size, circles.size/circles.variation);
         circles.size = constrain(circles.size, circles.minSize, circles.maxSize);
+        strokeWeight(3*circles.thickness);
+        let hue = lerp(circles.h, bg.h, 0,75);
+        let saturation = lerp(circles.s, bg.s, 0,75);
+        let brightness = lerp(circles.b, bg.b, 0,75);
+        stroke(hue, saturation, brightness);
+        ellipse(circles.x, circles.y, circles.size);
+        strokeWeight(circles.thickness);
+        stroke(circles.h, circles.s, circles.b);
         ellipse(circles.x, circles.y, circles.size);
         pop();
     }
 
-    // draws lines
-    lines.height = lerp(lines.minHeight, lines.maxHeight, 0.5); 
-    lines.spacing = lines.thickness*2;
-    for (lines.x = 0; lines.x <= width; lines.x += lines.spacing) {
-        push();
-        noFill();
-        strokeCap(ROUND);
-        strokeWeight(lines.thickness);
-        stroke(lines.h, lines.s, lines.b);
-        lines.y = constrain(lines.y, lines.minHeight, lines.maxHeight);
-        line(lines.x, height, lines.x, lines.y);
-        lines.y = randomGaussian(lines.height, lines.height/lines.variation);
-        pop();
+    // // draws lines
+    // lines.height = lerp(lines.minHeight, lines.maxHeight, 0.5); 
+    // lines.spacing = lines.thickness*2;
+    // for (lines.x = 0; lines.x <= width; lines.x += lines.spacing) {
+    //     push();
+    //     noFill();
+    //     strokeCap(ROUND);
+    //     strokeWeight(lines.thickness);
+    //     stroke(lines.h, lines.s, lines.b);
+    //     lines.y = constrain(lines.y, lines.minHeight, lines.maxHeight);
+    //     line(lines.x, height, lines.x, lines.y);
+    //     lines.y = randomGaussian(lines.height, lines.height/lines.variation);
+    //     pop();
+    // }
+
+    // draws base-layer of noise
+    for (let i = 0; i < noise.amount; i++) {
+        let x = randomGaussian(width/2, 1.5*width*noise.area);
+        let y = randomGaussian(height/2, 1.5*width*noise.area);
+        let hue = lerp(noise.hue, bg.h, 0.75);
+        let saturation = lerp(noise.saturation, bg.s, 0.75);
+        let brightness = lerp(noise.brightness, bg.b, 0.75);
+        stroke(hue, saturation, brightness);
+        strokeWeight(noise.pointSize);
+        point(x, y);
+    }
+
+    // draws mid-layer of noise
+    for (let i = 0; i < noise.amount; i++) {
+        let x = randomGaussian(width/2, 1.5*width*noise.area);
+        let y = randomGaussian(height/2, 1.5*width*noise.area);
+        let hue = lerp(noise.hue, bg.h, 0.5);
+        let saturation = lerp(noise.saturation, bg.s, 0.5);
+        let brightness = lerp(noise.brightness, bg.b, 0.5);
+        stroke(hue, saturation, brightness);
+        strokeWeight(noise.pointSize);
+        point(x, y);
     }
 
     // draws noise
