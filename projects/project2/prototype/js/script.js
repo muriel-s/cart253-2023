1c1c70d94b49f4a74249b4ca4de69c502fab34d0
muriel-s <muriel.liberatore@gmail.com>
2023-11-08 15:24:35 -0500

    P2: working line pattern and circle pattern

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -24,22 +24,38 @@ let noise = {
     hue: 180,
     saturation: 0,
     brightness: 100,
-    pointSize: 4,
+    pointSize: 2,
     area: 0.15
 }
 
-// line pattern properties
-let lines = {
+// circle pattern properties
+let circles = {
     thickness: 1,
     h: 180,
     s: 100,
     b: 100,
     x: 0,
     y: 0,
+    size: 0,
+    minSize: 250,
+    maxSize: 750,
+    spacing: 0,
+    amount: 4
+}
+
+// line pattern properties
+let lines = {
+    thickness: 2,
+    h: 180,
+    s: 100,
+    b: 100,
+    x: 0,
+    y: 0,
     height: 0,
-    minHeight: 250,
+    minHeight: 500,
     maxHeight: 1000,
     spacing: 0,
+    variation: 10
 }
 
 function setup() {
@@ -52,18 +68,34 @@ function draw() {
     // draws background
     background(bg.h, bg.s, bg.b);
 
+    // draws ellipse pattern
+    circles.size = lerp(circles.minSize, circles.maxSize, 0.5);
+    circles.spacing = circles.thickness*200;
+    for (let i = 0; i < circles.amount; i++) {
+        push();
+        noFill();
+        strokeWeight(circles.thickness);
+        stroke(circles.h, circles.s, circles.b);
+        circles.x = width/2;
+        circles.y = height/2;
+        circles.size = randomGaussian(circles.size, circles.size/10);
+        circles.size = constrain(circles.size, circles.minSize, circles.maxSize);
+        ellipse(circles.x, circles.y, circles.size);
+        pop();
+    }
+
     // draws lines
     lines.height = lerp(lines.minHeight, lines.maxHeight, 0.5); 
     lines.spacing = lines.thickness*2;
-    for (lines.x = 0; lines.x < width; lines.x += lines.spacing) {
+    for (lines.x = 0; lines.x <= width; lines.x += lines.spacing) {
         push();
         noFill();
         strokeCap(ROUND);
         strokeWeight(lines.thickness);
         stroke(lines.h, lines.s, lines.b);
-        lines.y = randomGaussian(lines.height, lines.height/10);
         lines.y = constrain(lines.y, lines.minHeight, lines.maxHeight);
         line(lines.x, height, lines.x, lines.y);
+        lines.y = randomGaussian(lines.height, lines.height/lines.variation);
         pop();
     }
 
