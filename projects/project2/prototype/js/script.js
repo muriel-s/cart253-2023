eda9b3b1be3303027a0b904dc670850166395a36
muriel-s <muriel.liberatore@gmail.com>
2023-11-08 17:22:44 -0500

    P2: drew a frame, need to make into function

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -18,6 +18,16 @@ let bg = {
     b: 50
 }
 
+// frame properties
+let frame = {
+    h: 0,
+    s: 50,
+    b: 50,
+    thickness: 200,
+    minThickness: 50,
+    width: 1000
+}
+
 // noise properties
 let noise = {
     amount: 2000,
@@ -28,35 +38,20 @@ let noise = {
     area: 0.15
 }
 
-// circle pattern properties
-let circles = {
-    thickness: 1,
-    h: 180,
-    s: 100,
-    b: 100,
-    x: 0,
-    y: 0,
-    size: 0,
-    minSize: 250,
-    maxSize: 1000,
-    spacing: 0,
-    amount: 1,
-    variation: 20
-}
-
-// // line pattern properties
-// let lines = {
-//     thickness: 2,
+// // circle pattern properties
+// let circles = {
+//     thickness: 1,
 //     h: 180,
 //     s: 100,
 //     b: 100,
 //     x: 0,
 //     y: 0,
-//     height: 0,
-//     minHeight: 500,
-//     maxHeight: 1000,
+//     size: 0,
+//     minSize: 250,
+//     maxSize: 1000,
 //     spacing: 0,
-//     variation: 10
+//     amount: 1,
+//     variation: 20
 // }
 
 function setup() {
@@ -67,61 +62,71 @@ function setup() {
 
 function draw() {
     // draws background
-    background(bg.h, bg.s, bg.b);
+    let backgroundColor = color(bg.h, bg.s, bg.b)
+    background(backgroundColor);
 
-    // // draws medium ellipse pattern
+    // draws frame
+    noFill();
+    rectMode(CENTER);
+    let frameColor = color(frame.h, frame.s, frame.b);
+    
+    let interA = lerpColor(frameColor, backgroundColor, 0.1);
+    let interB = lerpColor(frameColor, backgroundColor, 0.2);
+    let interC = lerpColor(frameColor, backgroundColor, 0.3);
+    let interD = lerpColor(frameColor, backgroundColor, 0.4);
+    let interE = lerpColor(frameColor, backgroundColor, 0.5);
+    let interF = lerpColor(frameColor, backgroundColor, 0.6);
+    
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.6));
+    stroke(interF);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.5));
+    stroke(interE);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.4));
+    stroke(interD);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.3));
+    stroke(interC);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.2));
+    stroke(interB);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(lerp(frame.minThickness, frame.thickness, 0.1));
+    stroke(interA);
+    rect(width/2, height/2, frame.width);
+
+    strokeWeight(frame.minThickness);
+    stroke(frameColor);
+    rect(width/2, height/2, frame.width);
+
+
+    // // draws ellipse pattern
     // circles.size = lerp(circles.minSize, circles.maxSize, 0.5);
     // circles.spacing = circles.thickness*20;
     // for (let i = 0; i < circles.amount; i++) {
     //     push();
     //     noFill();
-    //     strokeWeight(2*circles.thickness);
-    //     let hue = lerp(circles.h, bg.h, 0,5);
-    //     let saturation = lerp(circles.s, bg.s, 0,5);
-    //     let brightness = lerp(circles.b, bg.b, 0,5);
-    //     stroke(hue, saturation, brightness);
     //     circles.x = width/2;
     //     circles.y = height/2;
     //     circles.size = randomGaussian(circles.size, circles.size/circles.variation);
     //     circles.size = constrain(circles.size, circles.minSize, circles.maxSize);
+    //     // // back-level of ellipse
+    //     // strokeWeight(3*circles.thickness);
+    //     // let hue = lerp(circles.h, bg.h, 0,75);
+    //     // let saturation = lerp(circles.s, bg.s, 0,75);
+    //     // let brightness = lerp(circles.b, bg.b, 0,75);
+    //     // stroke(hue, saturation, brightness);
+    //     // ellipse(circles.x, circles.y, circles.size);
+    //     // normal ellipse
+    //     strokeWeight(circles.thickness);
+    //     stroke(circles.h, circles.s, circles.b);
     //     ellipse(circles.x, circles.y, circles.size);
-    //     pop();
-    // }
-
-    // draws ellipse pattern
-    circles.size = lerp(circles.minSize, circles.maxSize, 0.5);
-    circles.spacing = circles.thickness*20;
-    for (let i = 0; i < circles.amount; i++) {
-        push();
-        noFill();
-        circles.x = width/2;
-        circles.y = height/2;
-        circles.size = randomGaussian(circles.size, circles.size/circles.variation);
-        circles.size = constrain(circles.size, circles.minSize, circles.maxSize);
-        strokeWeight(3*circles.thickness);
-        let hue = lerp(circles.h, bg.h, 0,75);
-        let saturation = lerp(circles.s, bg.s, 0,75);
-        let brightness = lerp(circles.b, bg.b, 0,75);
-        stroke(hue, saturation, brightness);
-        ellipse(circles.x, circles.y, circles.size);
-        strokeWeight(circles.thickness);
-        stroke(circles.h, circles.s, circles.b);
-        ellipse(circles.x, circles.y, circles.size);
-        pop();
-    }
-
-    // // draws lines
-    // lines.height = lerp(lines.minHeight, lines.maxHeight, 0.5); 
-    // lines.spacing = lines.thickness*2;
-    // for (lines.x = 0; lines.x <= width; lines.x += lines.spacing) {
-    //     push();
-    //     noFill();
-    //     strokeCap(ROUND);
-    //     strokeWeight(lines.thickness);
-    //     stroke(lines.h, lines.s, lines.b);
-    //     lines.y = constrain(lines.y, lines.minHeight, lines.maxHeight);
-    //     line(lines.x, height, lines.x, lines.y);
-    //     lines.y = randomGaussian(lines.height, lines.height/lines.variation);
     //     pop();
     // }
 
