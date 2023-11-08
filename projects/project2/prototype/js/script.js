09ad71b8e0c9fb6733b81dcf442f5affa47243d3
muriel-s <muriel.liberatore@gmail.com>
2023-11-08 11:28:39 -0500

    P2: started base of prototype

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -1,6 +1,6 @@
 /**
- * Title of Project
- * Author Name
+ * Project 2 Prototype
+ * Muriel
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -8,25 +8,35 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
-
+let background = {
+    h: 180,
+    s: 50,
+    b: 50
 }
 
+let noise = {
+    amount: 1000,
+    hue: 180,
+    saturation: 100,
+    brightness: 100,
+    pointSize: 1,
+}
 
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(1000, 1000);
+    colorMode(HSB);
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+    // draws background
+    background(background.h, background.s, background.b);
 
+    // draws noise
+    for (let i = 0; i < noise.amount; i++) {
+        let x = random(0, width);
+        let y = random(0, height);
+        stroke(noise.hue, noise.saturation, noise.brightness);
+        strokeWeight(noise.pointSize);
+        point(x, y);
+    }
 }
