b5dbde827bf62c0ca62fa055d505b1bfdda9d788
muriel-s <muriel.liberatore@gmail.com>
2023-11-21 11:52:17 -0500

    P2: made text change color when mouse hovers over it

--- projects/project2/prototype/js/Quiz.js
+++ projects/project2/prototype/js/Quiz.js
@@ -1,6 +1,15 @@
 class Quiz {
     constructor() {
-
+        // text color properties
+        this.questionColor = color(60, 58, 94);
+        this.answerColor = 100;
+        this.answerSelectColor = color(60, 58, 94);
+        // text position properties
+        this.questionHeight = height/4;
+        this.answer1Height = 400;
+        this.answer2Height = 525;
+        this.answer3Height = 650;
+        this.answer4Height = 775;
     }
 
     draw() {
@@ -13,7 +22,7 @@ class Quiz {
     quizBackground() {
         // draws noise ...
         for (let i = 0; i < 200; i++) {
-            let randomColor = color(random(0, 360), random(0, 100), random(0, 100));
+            let randomColor = color(random(0, 360), random(50, 100), random(50, 100));
             stroke(randomColor);
             strokeWeight(3);
             // ... in top left corner
@@ -33,15 +42,6 @@ class Quiz {
             y = randomGaussian(height, width*0.15);
             point(x, y);
         };
-        // places star decorations
-        push();
-        tint(50, 40, 100, 50);
-        imageMode(CENTER);
-        image(deco1, 100, 200);
-        image(deco2, 800, 700);
-        image(deco3, 900, 300);
-        image(deco4, 300, 900);
-        pop();
     
         // draws starlight trail following mouse
         for (let i = 0; i < 10; i++) {
@@ -59,12 +59,40 @@ class Quiz {
         textFont('Pixelify Sans');
         rectMode(CORNER);
         textAlign(LEFT, BOTTOM);
-        fill(100);
         textSize(32);
-        text(`Question question question question?`, width/5, height/3, 2*width/3);
-        text(`Answer!`, width/5, 500, 3*width/4);
-        text(`Answer...`, width/5, 600, 3*width/4);
-        text(`Answer answer.`, width/5, 700, 3*width/4);
+        // question
+        fill(this.questionColor);
+        text(`Question question question question?`, width/5, this.questionHeight, 2*width/3);
+
+        // answer 1
+        if (mouseY > this.answer1Height - 25 && mouseY < this.answer1Height + 15) {
+            fill(this.answerSelectColor);
+        } else {
+            fill(this.answerColor);
+        }
+        text(`Answer!`, width/5, this.answer1Height, 3*width/4);
+        // answer 2
+        if (mouseY > this.answer2Height - 25 && mouseY < this.answer2Height + 15) {
+            fill(this.answerSelectColor);
+        } else {
+            fill(this.answerColor);
+        }
+        text(`Answer...`, width/5, this.answer2Height, 3*width/4);
+        // answer 3
+        if (mouseY > this.answer3Height - 25 && mouseY < this.answer3Height + 15) {
+            fill(this.answerSelectColor);
+        } else {
+            fill(this.answerColor);
+        }
+        text(`Answer answer.`, width/5, this.answer3Height, 3*width/4);
+        // answer 4
+        if (mouseY > this.answer4Height - 25 && mouseY < this.answer4Height + 15) {
+            fill(this.answerSelectColor);
+        } else {
+            fill(this.answerColor);
+        }
+        text(`Answer answer answer.`, width/5, this.answer4Height, 3*width/4);
+        pop();
     }
 
     cursorStar() {
--- projects/project2/prototype/js/Title.js
+++ projects/project2/prototype/js/Title.js
@@ -1,15 +1,50 @@
 class Title {
     constructor() {
-        this.titleString = `Muriel's fun little quiz`;
+        this.titleString = `Aura Generator`;
+        this.introText = `Click anywhere to begin`;
     }
 
     draw() {
         background(0); // later set a dark color-changing bg
         this.displayTitle();
+        this.displayIntroText();
     }
 
     displayTitle() {
+        push();
+        noStroke();
+        textFont('Pixelify Sans');
+        rectMode(CORNER);
+        textAlign(CENTER, BOTTOM);
+        textSize(72);
+        // red offset
+        fill(0, 100, 100);
+        text(this.titleString, width/2 - 2, height/3);
+        // cyan offset
+        fill(180, 100, 100);
+        text(this.titleString, width/2 + 2, height/3);
+        // white text
+        fill(100);
+        text(this.titleString, width/2, height/3);
+        pop();
+    }
 
+    displayIntroText() {
+        push();
+        noStroke();
+        textFont('Pixelify Sans');
+        rectMode(CORNER);
+        textAlign(CENTER, BOTTOM);
+        textSize(48);
+        // red offset
+        fill(0, 100, 100);
+        text(this.introText, width/2 - 1, height/2);
+        // cyan offset
+        fill(180, 100, 100);
+        text(this.introText, width/2 + 1, height/2);
+        fill(100);
+        text(this.introText, width/2, height/2);
+        pop();
     }
 
     mousePressed() {
